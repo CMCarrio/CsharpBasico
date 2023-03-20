14cc30d2e679780ec53c06908e4b9128f0421d90
EL-tincho <cristianmcarrio@gmail.com>
2023-03-20 02:32:10 -0300

    Excepciones

---
+++ Excepciones/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad;
+
+            try
+            {
+                Console.Write("Ingresa tu edad, Por favor: ");
+                Edad = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Erorr: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
